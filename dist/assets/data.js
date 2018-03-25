e23fd59e41cd464cf9dd6fe09692da5cf1c14f9e
celine <bonin.c.m@gmail.com>
2018-03-26 10:22:41 +1100

    working on prod export config

---
+++ dist/assets/data.js
@@ -0,0 +1,161 @@
+[{
+    "name": {
+    "title": "mrs",
+    "first": "olivia",
+    "last": "moreno"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/39.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "noah",
+    "last": "harris"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/92.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "vera",
+    "last": "jackson"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/29.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "mildred",
+    "last": "rice"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/79.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "brandie",
+    "last": "kim"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/31.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "dwayne",
+    "last": "watson"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/31.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "rafael",
+    "last": "weaver"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/98.jpg"
+  },
+  {
+    "name": {
+    "title": "miss",
+    "first": "jessie",
+    "last": "cunningham"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/89.jpg"
+  },
+  {
+    "name": {
+    "title": "ms",
+    "first": "harper",
+    "last": "chavez"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/60.jpg"
+  },
+  {
+    "name": {
+    "title": "miss",
+    "first": "cathy",
+    "last": "hawkins"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/16.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "tom",
+    "last": "hawkins"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/9.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "anthony",
+    "last": "burns"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/56.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "lance",
+    "last": "kelly"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/66.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "arthur",
+    "last": "barnes"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/96.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "katrina",
+    "last": "bell"
+  },
+    "profile": "https://randomuser.me/api/portraits/med/women/24.jpg"
+    },
+  {
+    "name": {
+    "title": "mr",
+    "first": "ben",
+    "last": "lee"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/28.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "amy",
+    "last": "wright"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/29.jpg"
+  },
+  {
+    "name": {
+    "title": "mrs",
+    "first": "marlene",
+    "last": "johnston"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/women/56.jpg"
+  },
+  {
+    "name": {
+    "title": "miss",
+    "first": "bonnie",
+    "last": "reyes"
+  },
+  "profile": "https://randomuser.me/api/portraits/med/women/68.jpg"
+  },
+  {
+    "name": {
+    "title": "mr",
+    "first": "leonard",
+    "last": "bennett"
+    },
+    "profile": "https://randomuser.me/api/portraits/med/men/46.jpg"
+  }
+]
